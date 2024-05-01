2496d11359a449761dcc1f2d12394539eed7589f
csslick <digitone@naver.com>
2024-05-02 01:18:04 +0900

    paging

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import GameTitle from "./lib/components/GameTitle.svelte";
+
+</script>
+
+<GameTitle title="Pair Game!" />
+
---
+++ src/lib/components/GameTitle.svelte
@@ -0,0 +1,28 @@
+<script>
+  import GamePlay from "./GamePlay.svelte";
+  import GameScore from "./GameScore.svelte";
+
+  export let title = "game title";
+  let page = "title"; // title, play, score
+</script>
+
+<main>
+  <!-- paging logic -->
+  {#if page === "title"}
+    <section>
+      <h1>{title}</h1>
+      <div class="btn-group">
+        <button 
+          on:click={() => page ="play"}
+        >Start</button>
+        <button
+          on:click={() => page="score"}
+        >Score</button>
+      </div>
+    </section>
+  {:else if page === "play"}
+    <GamePlay />
+  {:else if page === "score"}
+    <GameScore />
+  {/if}
+</main>
